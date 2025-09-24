b755a10146a8642c1e7987f3d6be05dd43667a6a
Victor Suarez Rovere <suarezvictor@gmail.com>
2025-09-24 16:17:10 -0300

    prepare blit demo - just software implementation

--- graphics.h
+++ graphics.h
@@ -4,26 +4,33 @@
 #ifndef __GRAPHICS_H__
 #define __GRAPHICS_H__
 
+#ifndef DISABLE_HARDWARE_ACCEL
 #ifdef VIDEO_FRAMEBUFFER_HRES
 #define FRAME_WIDTH VIDEO_FRAMEBUFFER_HRES
+#else
+#error VIDEO_FRAMEBUFFER_HRES not defined
 #endif
 #ifdef VIDEO_FRAMEBUFFER_VRES
 #define FRAME_HEIGHT VIDEO_FRAMEBUFFER_VRES
-#define FRAME_PITCH (FRAME_WIDTH<<2)
+#else
+#error VIDEO_FRAMEBUFFER_VRES not defined
 #endif
-
 #ifndef VIDEO_FRAMEBUFFER_BASE
-#define VIDEO_FRAMEBUFFER_BASE 0x40C00000 //default LiteX framebuffer memory address
+#error VIDEO_FRAMEBUFFER_BASE not defined
+#endif
+#else
+//DISABLE_HARDWARE_ACCEL  defined
+#ifndef FRAME_WIDTH
+#define FRAME_WIDTH 1366
+#define FRAME_HEIGHT 768
+#endif
+extern unsigned long VIDEO_FRAMEBUFFER_BASE;
 #endif
 
-#define rgba2raw(r, g, b, a) ((r) | ((g)<<8) | ((b)<<16) | ((a)<<24))
+#define FRAME_PITCH (FRAME_WIDTH<<2)
 
-#define alpha_mul(x, a) (((a)*(x))>>8) //for more precision: (x * a + 1 + ((x * a) >> 8)) >> 8;
 
-#define alpha_blend_i(r, r1, g, g1, b, b1, a_inv, a1) \
-   rgba2raw(r + alpha_mul(r1, a_inv), \
-            g + alpha_mul(g1, a_inv), \
-            b + alpha_mul(b1, a_inv), \
-            a1 - a_inv)
+#define rgba2raw(r, g, b, a) ((r&255) | ((g&255)<<8) | ((b&255)<<16) | ((a&255)<<24))
+#define alpha_mul(x, a) (((a)*(x))>>8) //factor is ideally 1 to 256 both inclusive
 
 #endif
--- sim_linux.c
+++ sim_linux.c
@@ -6,36 +6,45 @@
 
 #include "graphics.h"
 
-#define DISABLE_HARDWARE_ACCEL
 typedef union {
   uint32_t rgba;
   struct { uint32_t r:8; uint32_t g:8; uint32_t b:8; uint32_t a:8; };
 } pix_t;
 
 pix_t fb[FRAME_HEIGHT][FRAME_WIDTH];
-#undef VIDEO_FRAMEBUFFER_BASE
-#define VIDEO_FRAMEBUFFER_BASE (intptr_t)(&fb[0][0].rgba)
+unsigned long VIDEO_FRAMEBUFFER_BASE = (unsigned long) &fb;
 
 #include "accel_cores.c"
 #include "sim_fb.c"
-#include "drawing_test.c"
 
-#if defined (CSR_ACCEL_RECTANGLE_FILL32_BASE) || defined(CSR_ACCEL_ELLIPSE_FILL32_BASE)
+#if defined (CSR_ACCEL_RECTANGLE_FILL32_BASE) \
+	|| defined(CSR_ACCEL_LINE32_BASE) \
+	|| defined(CSR_ACCEL_LINE32A_BASE) \
+	|| defined(CSR_ACCEL_BLIT32_BASE) \
+	|| defined(CSR_ACCEL_ELLIPSE_FILL32_BASE)
 #error Accelerators should be disabled
 #endif
 
 fb_handle_t fbh;
 
+void graphics_app(void);
+void wait_vsync(void)
+{
+  fb_update(&fbh, fb, sizeof(fb[0]));
+  if(fb_should_quit(/*&fbh*/))
+  	exit(0);
+}
+
 int main()
 {
+	assert(sizeof(VIDEO_FRAMEBUFFER_BASE) == sizeof(void*));
+	
   fb_init(FRAME_WIDTH, FRAME_HEIGHT, 0, &fbh); //set pix_vblank parameter to true to limit FPS
   
-  draw_shapes(1000);
-  
-  while(!fb_should_quit())
+  while(!fb_should_quit(/*&fbh*/))
   {
-    draw_clock();
-    fb_update(&fbh, fb, sizeof(fb[0]));
+    graphics_app();
+    wait_vsync();
   }
 
   return 0;
