48edfdb6efec2e8af88664d8c39f27f9adbaa2a8
Victor Suarez Rovere <suarezvictor@gmail.com>
2026-08-18 15:21:24 -0300

    fix a failed global replacement string

--- freetype/FreeTypeAmalgam.c
+++ freetype/FreeTypeAmalgam.c
@@ -39105,7 +39105,7 @@ FT_BEGIN_HEADER
 #define TT_NAME_ID_FULL_NAME            4
 #define TT_NAME_ID_VERSION_STRING       5
 #define TT_NAME_ID_PS_NAME              6
-#define TT_NAME_ID_VERSION:            7
+#define TT_NAME_ID_TRADEMARK            7
 
   /* the following values are from the OpenType spec */
 #define TT_NAME_ID_MANUFACTURER         8
